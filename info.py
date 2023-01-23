55fa48ca227b7ce5c9f2b4c192c8fede1e94170e
Pavan844 <117534484+Pavan844@users.noreply.github.com>
2023-01-23 21:06:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,7 +36,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://VGJN:e6BUkjSoPCoAL0oU@cluster0.jigw8ao.mongodb.net/?retryWrites=true&w=majority
-DATABASE_NAME = environ.get('DATABASE_NAME', "cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "VGJN")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
