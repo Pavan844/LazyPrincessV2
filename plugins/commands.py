fbab1d7bbf50d963f49f1123501d9b39a9bda2b7
Pavan844 <117534484+Pavan844@users.noreply.github.com>
2023-01-23 23:08:45 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,9 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/@facts710
-
-')] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='https://youtube.com/@facts710')] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
